5e8ab6c8dcb716c37675b67d52a8cde3418bb40e
Cristhian <cristhianfernandolp@gmail.com>
2025-04-25 10:17:09 -0300

    feat(auth): add initial user button component

---
+++ components/auth/user-button.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { LogOutIcon } from "lucide-react";
+
+import {
+  DropdownMenu,
+  DropdownMenuItem,
+  DropdownMenuContent,
+  DropdownMenuTrigger
+} from "@/components/ui/dropdown-menu"
+
+import {
+  Avatar,
+  AvatarImage,
+  AvatarFallback
+} from "@/components/ui/avatar";
+import { UserIcon } from "lucide-react";
+import { useCurrentUser } from "@/hooks/use-current-user";
+import { LogoutButton } from "./logout-button";
+
+export const UserButton = () => {
+
+  const user = useCurrentUser();
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <Avatar>
+          <AvatarImage src={user?.image || ""} />
+          <AvatarFallback className="bg-neutral-600">
+            <UserIcon className="text-white size-4"/>
+          </AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <LogoutButton>
+          <DropdownMenuItem>
+            Logout
+            <LogOutIcon />
+          </DropdownMenuItem>
+        </LogoutButton>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
