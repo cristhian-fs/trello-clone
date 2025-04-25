2141e6601700d50f1934749edf80e9d4d31ef311
Cristhian <cristhianfernandolp@gmail.com>
2025-04-25 10:16:32 -0300

    chore: update hrefs for login pages

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -10,12 +10,12 @@ export const Navbar = () => {
         <Logo />
         <div className="space-x-4 md:block md:w-auto flex items-center justify-between w-full">
           <Button size="sm" variant="outline" asChild>
-            <Link href="/sign-in">
+            <Link href="/auth/login">
               Login
             </Link>
           </Button>
           <Button size="sm" asChild>
-            <Link href="/sign-up">
+            <Link href="/auth/register">
               Get Taskify for free
             </Link>
           </Button>
