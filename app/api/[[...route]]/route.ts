9aa166e6646cf59fd49d223fb84ca64f9d3341d6
Cristhian <cristhianfernandolp@gmail.com>
2025-04-29 13:57:40 -0300

    chore(db): update route handler

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -2,13 +2,17 @@ import { Hono } from "hono";
 import { handle } from "hono/vercel";
 import authRoutes from "@/features/auth/server/route"
 import orgsRoutes from "@/features/organizations/server/route"
+import membersRoutes from "@/features/members/server/route"
+import boardsRoutes from "@/features/boards/server/route"
 
 const app = new Hono().basePath('/api');
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const routes = app
   .route("/auth", authRoutes)
-  .route('/organizations', orgsRoutes);
+  .route('/organizations', orgsRoutes)
+  .route('/members', membersRoutes)
+  .route('/boards', boardsRoutes)
 
 export type AppType = typeof routes;
 
